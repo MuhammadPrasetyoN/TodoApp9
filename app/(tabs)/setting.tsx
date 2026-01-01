cba431ae99955c171215d389c3ee2a237413672f
MuhammadPrasetyoN <muhammad.prasetyo.2005356@students.um.ac.id>
2026-01-01 23:45:18 +0700

    initial commit

---
+++ app/(tabs)/setting.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const settingScreen = () => {
+  return (
+    <View>
+      <Text>settingScreen</Text>
+    </View>
+  );
+};
+
+export default settingScreen;
